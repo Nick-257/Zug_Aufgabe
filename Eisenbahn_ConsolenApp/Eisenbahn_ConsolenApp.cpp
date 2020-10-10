a41c584b57e4bc5b9b80c64eaac971b532ea4cd4
NickDa <66820706+Nick-257@users.noreply.github.com>
2020-10-10 17:34:51 +0200

    Add files via upload

--- Eisenbahn_ConsolenApp/Eisenbahn_ConsolenApp.cpp
+++ Eisenbahn_ConsolenApp/Eisenbahn_ConsolenApp.cpp
@@ -18,128 +18,26 @@
 #include <stack> // convert int to digits
 
 #include <limits>
+#include <typeinfo>
 
 #include "ZugWaggon.h"
 #include "ZugFahrt.h"
 
 using namespace std;
 
-
-
-#include <typeinfo>
-bool valideEingabe(int Num_PassagiereOut, int Num_Belegung) {
-    if (typeid(Num_PassagiereOut).name() == "int") {
-        return false;
-    }
-    else if (Num_PassagiereOut < 0) {
-        std::cout << "Keine Valide Zahl!" << endl;
-        return false;
-    }
-    else if ((Num_Belegung - Num_PassagiereOut) < 0) {
-        std::cout << "Es koennen nicht mehr Passagiere aussteigen, als eingestiegen sind!" << endl;
-        return false;
-    } else {
-        return true;
-    }
-}
-
-
-void ZugFahrtModellierung(ZugFahrt F2, vector<int> ZugKonfiguration_data) {
-    std::cout << "\n\nAn wie vielen Bahnhoefen haelt der Zug: ";
-    int AnzahlBahnhoefe;
-    cin >> AnzahlBahnhoefe;
-    while (!cin.good()) {
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        std::cout << "\n\nAn wie vielen Bahnhoefen haelt der Zug (Bitte eine ganze Zahl angeben): ";
-        cin >> AnzahlBahnhoefe;
-    }
-
-    int BahnhofNummer = 1;
-    int Belegung = 0;
-
-    while (BahnhofNummer <= AnzahlBahnhoefe) {
-        int MiliSeconds = rand() % 120000 + 200; // 2 Minuten sind 120000 Milisekunden
-        int Steps = MiliSeconds / 10;
-        double FahrZeit = (double)MiliSeconds / 1000;
-        std::cout.precision(2);
-        std::cout << "Die erwartete Fahrzeit betraegt " << fixed <<  FahrZeit << " Sekunden." << endl;
-        std::cout << " /\\                                            /\\" << endl << "| |";
-        int StepCounter = 0;
-        while (StepCounter <= 10) {
-            this_thread::sleep_for(chrono::milliseconds(Steps));
-            std::cout << "____";
-            StepCounter++;
-        }
-        std::cout << "| |" << endl << "\nDer naechste Bahnhof wurde erreicht!\n\n" << endl;
-
-        string Aufgabe;
-        while (Aufgabe != "Abfahrt" && Aufgabe != "Abf" && Aufgabe != "abf") {
-
-            std::cout << "Bitte Keyword eingeben: Passagiere (P), Status (S), Belegung (B), Abfahrt (Abf), Abbruch (Abb)" << endl;
-            cin >> Aufgabe;
-
-            if (Aufgabe == "Status" || Aufgabe == "S" || Aufgabe == "s") {
-                std::cout << F2.getStatus(ZugKonfiguration_data) << endl;
-                std::cout << "Im Zug befinden sich momentan " << Belegung << " Passagiere." << endl << endl;
-                continue;
-            }
-            else if (Aufgabe == "Passagiere" || Aufgabe == "P" || Aufgabe == "p") {
-                std::cout << "Wie viele Passagiere sind eingestiegen: ";
-                int PassagiereIn = 0;
-                int PassagiereOut = 0;
-                cin >> PassagiereIn;
-                while (!cin.good()) {
-                    cin.clear();
-                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                    cin >> PassagiereIn;
-                }
-                Belegung += PassagiereIn;
-                if (Belegung == 0 || BahnhofNummer == 1) {
-                    // Wenn keine Passagiere im Zug sind, kann auch niemand aussteigen!
-                    std::cout << "Keine Passagiere im Zug, die aussteigen koennten!" << endl;
-                    continue;
-                }
-                else {
-                    std::cout << "Wie viele Passagiere sind ausgestiegen: ";
-                    cin >> PassagiereOut;
-                    while (!cin.good()) {
-                        cin.clear();
-                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                        std::cout << "Bitte wiederholen Sie die Anzahl an ausgestiegenen Passagieren: ";
-                        cin >> PassagiereOut;
-                    }
-                    while (!valideEingabe(PassagiereOut, Belegung)) {
-                        std::cout << "Bitte wiederholen Sie die Anzahl an ausgestiegenen Passagieren: ";
-                        cin >> PassagiereOut;
-                        while (!cin.good()) {
-                            cin.clear();
-                            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                            std::cout << "Bitte wiederholen Sie die Anzahl an ausgestiegenen Passagieren: ";
-                            cin >> PassagiereOut;
-                        }
-                    }
-                    Belegung -= PassagiereOut;
-                }
-                continue;
-            }
-            else if (Aufgabe == "Belegung" || Aufgabe == "B" || Aufgabe == "b") {
-                std::cout << "Im Zug befinden sich momentan " << Belegung << " Passagiere" << endl;
-                continue;
-            }
-            else if (Aufgabe == "Abbruch" || Aufgabe == "Abb" || Aufgabe == "abb") {
-                std::cout << "Der Zug stoppt an diesem Bahnhof!" << endl;
-                BahnhofNummer = AnzahlBahnhoefe;
-                break;
-            }
-        }
-        std::cout << "Der Zug verlaesst den Bahnhof!" << endl;
-        BahnhofNummer++;
-    }
+void displayGreeting() {
+    std::cout << "\n\nHerzlich Willkommen zum Zug-Konfigurator.\n\n\n" << endl
+        << "*                                                  ***** \n"
+        "*                                                    **** \n"
+        "* __________   __________   __________          _____  ***\n"
+        "*|          | |          | |          |  ___.___|   |___\\/_,,\n"
+        "*|__________| |__________| |__________| |_______||___________|\n"
+        "* OO      OO   OO      OO   OO    OO     OO  OO ||  OO     OO\n\n\n" << endl
+        << "\n\nBitte den Namen des Zugfuhrers angeben: ";
 }
 
 bool getStarted(const string& StartInput) {
-    if (StartInput == "no") {
+    if (StartInput == "Nein") {
         return false;
     }
     else {
@@ -148,10 +46,8 @@ bool getStarted(const string& StartInput) {
 }
 
 int main(int argc, const char* argv[]) {
+    displayGreeting();
 
-
-    // Neue Zugfahrt anlegen.
-    std::cout << "Bitte den Namen des Zugfuhrers angeben: ";
     string Name_Lokfuehrer;
     getline(cin, Name_Lokfuehrer);
     ZugFahrt F1(Name_Lokfuehrer);
@@ -159,15 +55,15 @@ int main(int argc, const char* argv[]) {
     vector<int> ZugKonfiguration = F1.ZugZusammenStellen();
 
 
-    string StartInput = "no";
+    string StartInput = "Nein";
     while (!getStarted(StartInput)) {
         std::cout << "Die ausgewaehlte Konfiguration ist:\n\n " << endl
-            << F1.getStatus(ZugKonfiguration) << "\n\n" << "Soll mit dieser Konfiguration gestartet werden? (Yes/no): ";
+            << F1.getStatus(ZugKonfiguration) << "\n\n" << "Soll mit dieser Konfiguration gestartet werden? (Ja/Nein): ";
         getline(cin, StartInput);
         while (F1.getValidUserInput(StartInput)) {
             getline(cin, StartInput);
         }
-        if (StartInput == "Yes") {
+        if (StartInput == "Ja") {
             break;
         }
         else {
@@ -176,14 +72,9 @@ int main(int argc, const char* argv[]) {
         }
     }
         
-    ZugFahrtModellierung(F1, ZugKonfiguration);
-
-    std::cout << "\n\n\nDer letzte Bahnhof wurde erreicht, alle Passagiere steigen hier aus!" << endl;
-    std::cout << "\n\n\nVielen Dank und bis zum naechsten mal!" << endl;
-    std::cout << "\nDas Programm endet automatisch in 30 Sekunden." << endl;
-    this_thread::sleep_for(chrono::milliseconds(30000));
-     
+    F1.ZugFahrtModellierung(F1, ZugKonfiguration);
+    F1.printFahrt();
+    F1.displayEnde();
 
     return 0;
-
 }
